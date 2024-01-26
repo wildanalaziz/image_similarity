d4100db7e54206ff1394231fe19c3d9f25079f60
Achmad Wildan <wildanalaziz82@gmail.com>
2024-01-26 14:24:16 +0700

    delete unused line

--- app.py
+++ app.py
@@ -19,13 +19,3 @@ if proses:
     if arrival is not None:
         with col2:
             st.image(arrival, caption="Arrival Photo")
-
-    # col3, col4 = st.columns(2)
-    # if origin is not None:
-    #     origin_fg = rembg.remove(Image.open(origin))
-    #     with col3:
-    #         st.image(origin_fg, caption="Origin Photo")
-    # if arrival is not None:
-    #     arrival_fg = rembg.remove(Image.open(arrival))
-    #     with col4:
-    #         st.image(rembg.remove(arrival_fg), caption="Arrival Photo")
